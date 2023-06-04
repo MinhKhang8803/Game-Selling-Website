739e092de4ae6cc418a071e12764fd0a6d027051
Minh Khang <khangbmgch210522@fpt.edu.vn>
2023-06-04 10:38:00 +0700

    Initial commit

---
+++ Assignment2/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Category()
+        {
+            return View();
+        }
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        public IActionResult EditCategory()
+        {
+            return View();
+        }
+        public IActionResult Manager()
+        {
+            return View();
+        }
+        public IActionResult AddManager()
+        {
+            return View();
+        }
+        public IActionResult EditManager()
+        {
+            return View();
+        }
+    }
+}
